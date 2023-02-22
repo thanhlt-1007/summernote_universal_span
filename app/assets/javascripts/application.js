44e9d3412f72f5e5a0f16cf513392492fd2451c3
le.tan.thanh <le.tan.thanh@sun-asterisk.com>
2023-02-22 12:41:20 +0700

    Edit poper image toolbar name

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,5 @@
 //= require summernote-image-attributes/summernote-image-attributes
 //= require summernote-image-attributes/lang/ja-JP
+
+//= require summernote-linkEditor/summernote-linkEditor
+//= require summernote-linkEditor/lang/ja-JP
---
+++ app/assets/javascripts/summernote-linkEditor/lang/ja-JP.js
@@ -0,0 +1,10 @@
+$.extend(true,$.summernote.lang,{
+  'ja-JP':{ /* Japanese */
+    linkEditor: {
+      email: "Email",
+      address: "Email address",
+      subject: "Subject line",
+      body: "Body text"
+    }
+  }
+});
